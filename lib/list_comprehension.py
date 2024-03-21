409eb39bf648926077830cfe2f3f67dffaa616c5
Phil <philrsheridan@gmail.com>
2024-03-21 10:49:02 -0400

    passing all tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    #interesting that n is needed at the start, and if without par
+    new_list = [n for n in num_list if n % 2 == 0]
+    return new_list
 
 def make_exclamation(sentence_list):
-    pass
+    new_list = [n + "!" for n in sentence_list if n != None]
+    return new_list
